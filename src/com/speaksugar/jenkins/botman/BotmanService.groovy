944a6776bfd86dfaa88272794d7555710123ff87
jeffries.yu <jeffries.yu@ringcentral.com>
2023-11-17 11:24:54 +0800

    fix: fix BotmanService.groovy

--- src/com/speaksugar/jenkins/botman/BotmanService.groovy
+++ src/com/speaksugar/jenkins/botman/BotmanService.groovy
@@ -3,6 +3,7 @@ package com.speaksugar.jenkins.botman
 import com.speaksugar.jenkins.util.HttpUtil
 
 class BotmanService {
+
     String botmanBaseUrl
     String botmanToken
 
@@ -12,10 +13,6 @@ class BotmanService {
      * @param message
      */
     void sendMessageToTeam(String teamId, String message) {
-        sendMessageToTeam([
-                'teamId': teamId,
-                'message': message,
-        ])
         HttpUtil.post(botmanBaseUrl + '/v2/user/message', [
                 'teamId': teamId,
                 'message': message,
